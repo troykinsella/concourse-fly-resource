1a374a8e4e72073fbfd05b5cf7fc6023535975f6
Troy Kinsella <troy.kinsella@startmail.com>
2018-02-09 15:08:27 -0800

    Betterness

--- assets/common.sh
+++ assets/common.sh
@@ -9,11 +9,11 @@ fetch_fly() {
   local insecure_arg=""
   test "$insecure" = "true" && insecure_arg="--insecure"
 
-  test -x $FLY || {
+  if ! [ -x $FLY ]; then
     echo "Fetching fly..."
     curl -SsL $insecure_arg -u "$username:$password" "$url/api/v1/cli?arch=amd64&platform=linux" -o $FLY
     chmod +x $FLY
-  }
+  fi
 }
 
 login() {
@@ -28,17 +28,14 @@ login() {
   test "$insecure" = "true" && insecure_arg="--insecure"
 
   echo "Logging in..."
-  (
-    set +e
-    local out=$($FLY login -t main $insecure_arg -c $url -n $team --username=$username --password=$password 2>&1)
+  local out=$($FLY login -t main $insecure_arg -c $url -n $team --username=$username --password=$password 2>&1)
 
-    # This sucks
-    if echo "$out" | grep "fly -t main sync" > /dev/null; then
-      test -n "$tried" && return 1
-      fetch_fly $url $username $password $insecure
-      login $url $username $password $team $insecure yes
-    fi
-  )
+  # This sucks
+  if echo "$out" | grep "fly -t main sync" > /dev/null; then
+    test -n "$tried" && return 1
+    fetch_fly $url $username $password $insecure
+    login $url $username $password $team $insecure yes
+  fi
 }
 
 init_fly() {
