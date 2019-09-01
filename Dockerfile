f2d6f7c4773ea11f40acdcfd2977e846c75e0828
Troy Kinsella <troykinsella@users.noreply.github.com>
2019-09-01 13:58:23 -0600

    General improvments (#14)
    * Use metadata defaults
    * Fix target
    * Fix docs

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,8 @@ LABEL MAINTAINER="Troy Kinsella <troy.kinsella@gmail.com>"
 COPY assets/* /opt/resource/
 
 RUN apk add --no-cache \
-    bash \
-    ca-certificates \
-    curl \
-    jq ;\
+      bash \
+      ca-certificates \
+      curl \
+      jq; \
     update-ca-certificates
--- assets/common.sh
+++ assets/common.sh
@@ -9,42 +9,42 @@ fetch_fly() {
 
   if ! [ -x $FLY ]; then
     echo "Fetching fly..."
-    curl -SsL $insecure_arg "$url/api/v1/cli?arch=amd64&platform=linux" -o $FLY
-    chmod +x $FLY
+    curl -fSsL $insecure_arg "$url/api/v1/cli?arch=amd64&platform=linux" -o "$FLY"
+    chmod +x "$FLY"
   fi
 }
 
 login() {
-  local url=$1
-  local username=$2
-  local password=$3
-  local team=$4
-  local insecure=$5
-  local target=$6
-  local tried=$7
+  local url="$1"
+  local username="$2"
+  local password="$3"
+  local team="$4"
+  local insecure="$5"
+  local target="$6"
+  local tried="$7"
 
   local insecure_arg=""
   test "$insecure" = "true" && insecure_arg="--insecure"
 
   echo "Logging in..."
-  local out=$($FLY login -t $target $insecure_arg -c $url -n $team --username=$username --password=$password 2>&1)
+  local out=$($FLY login -t "$target" $insecure_arg -c "$url" -n "$team" "--username=$username" "--password=$password" 2>&1)
 
   # This sucks
   if echo "$out" | grep "fly -t $target sync" > /dev/null; then
     test -n "$tried" && return 1
-    fetch_fly $url $insecure
-    login $url $username $password $team $insecure $target yes
+    fetch_fly "$url" "$insecure"
+    login "$url" "$username" "$password" "$team" "$insecure" "$target" yes
   fi
 }
 
 init_fly() {
-  local url=$1
-  local username=$2
-  local password=$3
-  local team=$4
-  local insecure=$5
-  local target=$6
+  local url="$1"
+  local username="$2"
+  local password="$3"
+  local team="$4"
+  local insecure="$5"
+  local target="$6"
 
-  fetch_fly $url $insecure
-  login $url $username $password $team $insecure $target
+  fetch_fly "$url" "$insecure"
+  login "$url" "$username" "$password" "$team" "$insecure" "$target"
 }
